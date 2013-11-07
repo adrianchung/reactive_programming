96a412af6ab474ed23718e24953d632fa5eda211
Adrian Chung <adrian.c.chung@gmail.com>
2013-11-07 08:53:58 -0500

    Initial checkin with failing work

---
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,29 @@
+package quickcheck
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+  
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[H]
+    m <- oneOf(empty, genHeap)
+  } yield m
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+}
